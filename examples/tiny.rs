d618ae32cabf46d2735dcb1bd9d58b9c7283daf1
Laurent <laurent.mazare@gmail.com>
2024-05-12 20:20:56 +0200

    Move the llama bits in their own file.

--- examples/tiny.rs
+++ examples/tiny.rs
@@ -1,10 +1,10 @@
 extern crate glim;
 
 fn main() -> anyhow::Result<()> {
-    let config = glim::Config::tiny_15m();
+    let config = glim::llama::Config::tiny_15m();
     // Converted from https://huggingface.co/karpathy/tinyllamas/blob/main/stories15M.pt
-    let model = glim::Model::new(config, "stories15M.safetensors")?;
-    let mut state = glim::State::new(1, 1, model.config())?;
+    let model = glim::llama::Model::new(config, "stories15M.safetensors")?;
+    let mut state = glim::llama::State::new(1, 1, model.config())?;
     model.fwd(&[42], &mut state)?;
     println!("logits: {:?}", &state.logits().data()[..20]);
     Ok(())
