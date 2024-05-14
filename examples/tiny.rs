b93e5d0d79f7ed6b5babcd7591f09df7bcc6993b
laurent <laurent.mazare@gmail.com>
2024-05-14 17:07:42 +0200

    Cosmetic change.

--- examples/tiny.rs
+++ examples/tiny.rs
@@ -4,8 +4,9 @@ fn main() -> anyhow::Result<()> {
     let config = glim::llama::Config::tiny_15m();
     // Converted from https://huggingface.co/karpathy/tinyllamas/blob/main/stories15M.pt
     let model = glim::llama::Model::new(config, "stories15M.safetensors")?;
-    let mut state = glim::llama::State::new(1, 2, model.config())?;
-    model.fwd(&[42, 1337], &mut state)?;
+    let tokens = vec![42, 1337, 1, 2, 3, 4];
+    let mut state = glim::llama::State::new(1, tokens.len(), model.config())?;
+    model.fwd(&tokens, &mut state)?;
     println!("logits: {:?}", &state.logits().data()[..20]);
     #[cfg(feature = "candle")]
     {
