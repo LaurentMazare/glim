cdb6e51cb52c4276376f5547731bee6d505a4a94
laurent <laurent.mazare@gmail.com>
2024-06-03 10:09:02 +0200

    index-select kernel.

--- cuda-kernels/indexing.cu
+++ cuda-kernels/indexing.cu
@@ -1,27 +1,37 @@
+#include "cuda_fp16.h"
+#include "cuda_bf16.h"
 #include<stdint.h>
 
 template<typename T, typename I>
 __device__ void index_select(
-    const size_t numel,
-    const size_t num_dims,
-    const size_t *info,
+    const int32_t numel,
+    const int32_t dim,
     const I *ids,
-    const T *inp,
-    T *out,
-    const size_t left_size,
-    const size_t src_dim_size,
-    const size_t ids_dim_size,
-    const size_t right_size
+    const T *src,
+    T *dst
 ) {
-    const size_t *dims = info;
-    const size_t *strides = info + num_dims;
-    bool b = is_contiguous(num_dims, dims, strides);
-    for (unsigned int dst_i = blockIdx.x * blockDim.x + threadIdx.x; dst_i < numel; dst_i += blockDim.x * gridDim.x) {
-          unsigned int left_i = dst_i / (ids_dim_size * right_size);
-          unsigned int id_i = dst_i / right_size % ids_dim_size;
-          unsigned int right_i = dst_i % right_size;
-          unsigned int src_i = left_i * (src_dim_size * right_size) + ids[id_i] * right_size + right_i;
-          unsigned strided_i = b ? src_i : get_strided_index(src_i, num_dims, dims, strides);
-          out[dst_i] = inp[strided_i];
+    int32_t i = blockIdx.x * blockDim.x + threadIdx.x;
+    int32_t j = blockIdx.y * blockDim.y + threadIdx.y;
+    if (i >= numel || j >= dim) {
+      return;
     }
+    dst[i * dim + j] = src[ids[i] * dim + j];
 }
+
+#define IS_OP(TYPENAME, INDEX_TYPENAME, FN_NAME) \
+extern "C" __global__ void FN_NAME(  \
+    const int32_t numel,  \
+    const int32_t dim, \
+    const INDEX_TYPENAME *ids, \
+    const TYPENAME *src, \
+    TYPENAME *dst \
+) { index_select(numel, dim, ids, src, dst); } \
+
+#if __CUDA_ARCH__ >= 800
+IS_OP(__nv_bfloat16, uint32_t, is_u32_bf16);
+#endif
+#if __CUDA_ARCH__ >= 530
+IS_OP(__half, uint32_t, is_u32_f16);
+#endif
+
+IS_OP(float, uint32_t, is_u32_f32);
