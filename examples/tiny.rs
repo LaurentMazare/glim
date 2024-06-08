31703dea84ca62f7197f36ff371f615fee3aaaac
laurent <laurent.mazare@gmail.com>
2024-06-08 10:02:32 +0100

    Make the example code more device generic.

--- examples/tiny.rs
+++ examples/tiny.rs
@@ -1,8 +1,11 @@
 extern crate glim;
 use anyhow::Context;
+use glim::Backend;
 
 use rand::{distributions::Distribution, SeedableRng};
 use tokenizers::Tokenizer;
+
+type B = Vec<f32>;
 
 fn main() -> anyhow::Result<()> {
     #[cfg(feature = "candle")]
@@ -16,18 +19,20 @@ fn main() -> anyhow::Result<()> {
     let mut rng = rand::rngs::StdRng::seed_from_u64(42424242);
 
     let config = glim::llama::Config::tiny_15m();
-    let mut prs = vec![0f32; config.vocab_size];
+    let vocab_size = config.vocab_size;
     // Converted from https://huggingface.co/karpathy/tinyllamas/blob/main/stories15M.pt
     let model = glim::llama::Model::new(config, &(), "stories15M.safetensors")?;
     let mut state = glim::llama::State::new(1, model.config(), &())?;
     let start_time = std::time::Instant::now();
     let bos_token = tokenizer.token_to_id("<s>").context("no bos token")?;
     let mut tokens = vec![bos_token];
+    let mut prs_storage = unsafe { B::alloc_uninit(vocab_size, &())? };
     for _ in 0..200 {
         let prev_token = tokens.last().unwrap();
         model.fwd(&[*prev_token], &mut state)?;
-        let prs = state.logits().softmax(&mut prs)?;
-        let distr = rand::distributions::WeightedIndex::new(prs.storage())?;
+        let prs = state.logits().softmax(&mut prs_storage)?;
+        let prs = prs.data()?;
+        let distr = rand::distributions::WeightedIndex::new(prs.as_ref())?;
         let token = distr.sample(&mut rng) as u32;
         tokens.push(token);
     }
