b63d956735aad3686c3dd5609912096ca8181107
Laurent <laurent.mazare@gmail.com>
2025-12-31 19:29:12 +0100

    Fix some clippy lints.

--- src/shape.rs
+++ src/shape.rs
@@ -430,7 +430,7 @@ fn hole_size(el_count: usize, prod_d: usize, s: &dyn std::fmt::Debug) -> Result<
     if prod_d == 0 {
         anyhow::bail!("cannot reshape tensor of {el_count} elements to {s:?}")
     }
-    if el_count % prod_d != 0 {
+    if !el_count.is_multiple_of(prod_d) {
         anyhow::bail!("cannot reshape tensor with {el_count} elements to {s:?}")
     }
     Ok(el_count / prod_d)
